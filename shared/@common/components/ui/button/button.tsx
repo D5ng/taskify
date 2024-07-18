915ab7cbce1665a10ae21dc08ed579c6e45d65f6
d5ng <dong5ffice@gmail.com>
2024-07-18 19:41:11 +0900

    [#3] ✨ Feat: Button 컴포넌트 추가

---
+++ shared/@common/components/ui/button/button.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image"
+import Loading from "../loading/loading"
+import { ButtonProps } from "./button.type"
+import classes from "./button.module.css"
+
+export default function Button(props: ButtonProps) {
+  const className = `${classes.button} ${props.size && classes[props.size]} ${classes[props.buttonStyle]} ${
+    props.fontColor && classes[props.fontColor]
+  }
+  `
+
+  return (
+    <button
+      className={className}
+      style={props.customStyle}
+      disabled={props.isDisabled || props.isLoading}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
+      {props.iconUrl && (
+        <div className={classes["icon-wrapper"]}>
+          <Image src={props.iconUrl} alt="" fill />
+        </div>
+      )}
+      {props.isLoading ? <Loading /> : props.children}
+    </button>
+  )
+}
