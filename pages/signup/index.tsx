166220c95db79e720e310ccf163057fc80fef228
d5ng <dong5ffice@gmail.com>
2024-07-19 00:20:21 +0900

    ✨ [#5] Feat: 회원가입 페이지 추가

---
+++ pages/signup/index.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+import AuthHeader from "@/features/auth/components/header/auth-header"
+import SignUpForm from "@/features/auth/components/form/auth-signup-form"
+import classes from "./index.module.css"
+
+export default function SignUpPage() {
+  return (
+    <section className={classes.section}>
+      <AuthHeader />
+      <SignUpForm />
+      <p className={classes.pathTo}>
+        이미 가입하셨나요? <Link href="/signin">로그인하기</Link>
+      </p>
+    </section>
+  )
+}
