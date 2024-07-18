03e133e3f7a778ad37530dbadb9f5049cc0f2ca1
d5ng <dong5ffice@gmail.com>
2024-07-18 20:19:48 +0900

    🎨  [#5] Style: hook 다시 내보내기

--- features/auth/hooks/index.ts
+++ features/auth/hooks/index.ts
@@ -1,6 +1,4 @@
-import useSignin from "./use-signin";
-import useSignup from "./use-signup";
+import useSignin from "./use-signin"
+import useSignup from "./use-signup"
 
-export {
-	useSignin, useSignup
-}
+export { useSignin, useSignup }
