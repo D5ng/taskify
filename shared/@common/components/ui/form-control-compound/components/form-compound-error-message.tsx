af27aa4e8c7e4639f9b8cb7d2f944f5b74a55cb0
d5ng <dong5ffice@gmail.com>
2024-07-20 00:18:30 +0900

    🔥 [#1] Remove: 파일 삭제

--- shared/@common/components/ui/form-control-compound/components/form-compound-error-message.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from "react"
-import { useFormControlContext } from "../form-control"
-import classes from "../form-control.module.css"
-
-export default function ErrorMessage(props: PropsWithChildren) {
-  const formControlContext = useFormControlContext()
-  if (formControlContext.hasError) return <p className={classes.errorMessage}>{props.children}</p>
-  return null
-}
--- shared/@common/components/ui/form-control-compound/components/index.ts
+++
@@ -1,5 +0,0 @@
-import Input from "./form-compound-input"
-import Label from "./form-compound-label"
-import ErrorMessage from "./form-compound-error-message"
-
-export { Input, Label, ErrorMessage }
