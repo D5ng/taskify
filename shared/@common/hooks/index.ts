60bfbc77af93e890193617525f72efe94ec30255
d5ng <dong5ffice@gmail.com>
2024-07-19 23:30:19 +0900

    ✨ [#11] Feat: useOutside 훅 추가

--- shared/@common/hooks/index.ts
+++ shared/@common/hooks/index.ts
@@ -1,7 +1,8 @@
 import useInput from "./use-input"
 import useToggle from "./use-toggle"
 import useRouterQuery from "./use-router-query"
+import useOutside from "./use-outside"
 
 export * from "./store"
 
-export { useInput, useToggle, useRouterQuery }
+export { useInput, useToggle, useRouterQuery, useOutside }
