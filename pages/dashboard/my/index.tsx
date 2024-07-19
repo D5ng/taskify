6da230a9cba604f104b80e7b045ca0e484e1457d
d5ng <dong5ffice@gmail.com>
2024-07-20 01:56:09 +0900

    ✨ [#18] Feat: 대시보드 페이지에 대시보드 리스트 추가

--- pages/dashboard/my/index.tsx
+++ pages/dashboard/my/index.tsx
@@ -1,9 +1,14 @@
+import { DashboardList } from "@/features/dashboard/dashboard-list/components"
+import { DashboardLayout } from "@dashboard/dashboard-layout"
 import { DashboardHeader } from "@dashboard/dashboard-header"
 
 export default function MyDashboard() {
   return (
     <>
       <DashboardHeader />
+      <DashboardLayout>
+        <DashboardList />
+      </DashboardLayout>
     </>
   )
 }
