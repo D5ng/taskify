25cc6747fbc0d987095344291460bc4cb2099687
d5ng <dong5ffice@gmail.com>
2024-07-23 02:30:44 +0900

    ✨ [#7] Feat: 대시보드 생성 모달 컴포넌트 추가

---
+++ shared/@dashboard/modal/create-dashboard-modal.tsx
@@ -0,0 +1,53 @@
+import Modal from "@common/components/ui/modal"
+import { useDashboardPageStore, useInput } from "@common/hooks"
+import { useColorChipSelect, useCreateDashboardForm } from "@dashboard/hooks"
+import { ColorChipColor } from "@common/types"
+import { isNotEmptyValidation } from "@common/utils/validation"
+import { ColorChipList } from "@common/components/ui/chips"
+import { FormControlDashboardName } from "@dashboard/form-control"
+
+interface Props {
+  isToggle: boolean
+  onCloseModal: () => void
+}
+
+export default function CreateDashboardModal(props: Props) {
+  const inputStates = useInput(isNotEmptyValidation)
+  const colorChipStates = useColorChipSelect<ColorChipColor>("#7AC555")
+  const currentPage = useDashboardPageStore.use.currentPage()
+
+  const onCloseModal = () => {
+    colorChipStates.handleResetColorChip()
+    props.onCloseModal()
+  }
+
+  const formStates = useCreateDashboardForm({
+    currentPage,
+    title: inputStates.inputValue,
+    color: colorChipStates.selectedColorChip,
+    onCloseModal,
+  })
+
+  const modalValues = {
+    ...formStates,
+    title: "대시보드 생성하기",
+    onCloseModal,
+  }
+
+  return (
+    props.isToggle && (
+      <Modal value={modalValues}>
+        <Modal.Backdrop />
+        <Modal.Form>
+          <Modal.Title />
+          <FormControlDashboardName {...inputStates} type="modal" id="dashboard-name" />
+          <ColorChipList {...colorChipStates} />
+          <Modal.ButtonLayout>
+            <Modal.OutlineButton>취소</Modal.OutlineButton>
+            <Modal.PrimaryButton>생성</Modal.PrimaryButton>
+          </Modal.ButtonLayout>
+        </Modal.Form>
+      </Modal>
+    )
+  )
+}
