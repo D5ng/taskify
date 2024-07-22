5c30e3afb34c608abb796963cecff1fce118c391
d5ng <dong5ffice@gmail.com>
2024-07-23 02:32:15 +0900

    ✨ Feat: document.tsx Portal을 위한 태그 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <div id="backdrop"></div>
+        <div id="modal"></div>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
