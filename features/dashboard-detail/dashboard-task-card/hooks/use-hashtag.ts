de377ed4cfe5281447d139f63b2f40f6f9f551e9
d5ng <dong5ffice@gmail.com>
2024-07-24 03:26:33 +0900

    ✨ [#32] Feat: Task Card 훅 추가

---
+++ features/dashboard-detail/dashboard-task-card/hooks/use-hashtag.ts
@@ -0,0 +1,41 @@
+import { getRandomColor } from "@/features/dashboard-detail/dashboard-task-card/utils"
+import { ChangeEventHandler, KeyboardEventHandler, useState } from "react"
+
+export default function useHashtag(defaultHashtags?: string[]) {
+  const [inputValue, setInputValue] = useState("")
+  const [hashtags, setHashtags] = useState<string[]>(defaultHashtags || [])
+
+  const handleInputValueChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> = (event) =>
+    setInputValue(event.target.value)
+
+  const handleKeyUp: KeyboardEventHandler<HTMLInputElement> = (event) => {
+    const allowedCommand = ["Comma", "Enter", "Space", "NumpadEnter"]
+    if (!allowedCommand.includes(event.code)) return
+    const target = event.target as HTMLInputElement
+
+    if (!target.value.trim().length) return
+
+    const { color, background } = getRandomColor()
+    const newTag = target.value.trim() + `$${color}$${background}`
+
+    setHashtags((prevState) => [...prevState, newTag])
+    setInputValue("")
+  }
+
+  const handleKeyDown: KeyboardEventHandler<HTMLInputElement> = (event) => {
+    if (event.code === "Enter") {
+      event.preventDefault()
+      return
+    }
+  }
+
+  return {
+    inputValue,
+    hashtags,
+    handleInputValueChange,
+    handleKeyDown,
+    handleKeyUp,
+    hasError: false,
+    isValid: true,
+  }
+}
---
+++ features/dashboard-detail/dashboard-task-card/hooks/use-task-card-form.ts
@@ -0,0 +1,67 @@
+import { FormEventHandler } from "react"
+import { AxiosResponse } from "axios"
+import { SWRMutationResponse } from "swr/mutation"
+import { useRouterQuery } from "@common/hooks"
+import { isImageValidation } from "@common/utils/validation"
+import { CreateTaskCard } from "@shared/dashboard/types"
+
+interface UseTaskCardFrom {
+  columnId: number
+  manager: number
+  title: string
+  description: string
+  deadline: string
+  hashtags: string[]
+  image: string | null
+  onCloseModal: () => void
+  mutation: SWRMutationResponse<AxiosResponse<any, any>, any, any, CreateTaskCard>
+}
+
+export default function useTaskCardForm({ onCloseModal, mutation, ...state }: UseTaskCardFrom) {
+  const dashboardId = useRouterQuery("id")
+
+  const onSubmit: FormEventHandler = async (event) => {
+    event.preventDefault()
+
+    const isValidation = isImageValidation(state)
+    let data: CreateTaskCard | null = null
+
+    if (isValidation && state.image) {
+      data = {
+        columnId: state.columnId,
+        assigneeUserId: state.manager,
+        title: state.title,
+        description: state.description,
+        dueDate: state.deadline,
+        tags: state.hashtags,
+        imageUrl: state.image,
+        dashboardId: +dashboardId,
+      }
+    }
+
+    if (isValidation && !state.image) {
+      data = {
+        columnId: state.columnId,
+        assigneeUserId: state.manager,
+        title: state.title,
+        description: state.description,
+        dueDate: state.deadline,
+        tags: state.hashtags,
+        dashboardId: +dashboardId,
+      }
+    }
+
+    try {
+      if (!data) throw new Error("Data 형식이 잘못되었습니다.")
+      await mutation.trigger(data!)
+      onCloseModal()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return {
+    onSubmit,
+    isLoading: mutation.isMutating,
+  }
+}
