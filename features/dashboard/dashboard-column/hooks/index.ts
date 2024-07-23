bcf579f29485a16a843be7db0dee171febc828ec
d5ng <dong5ffice@gmail.com>
2024-07-23 23:43:35 +0900

    ✨ [#31] Feat: 대시보드 컬럼 컴포넌트 추가

---
+++ features/dashboard/dashboard-column/hooks/index.ts
@@ -0,0 +1,7 @@
+import useDeleteDashboardColumnForm from "./use-delete-dashboard-column-form"
+import useEditDashboardColumnForm from "./use-edit-dashboard-column-form"
+
+export * from "./queries/dashboard-column.query"
+export * from "./mutation/dashboard-column.mutation"
+
+export { useDeleteDashboardColumnForm, useEditDashboardColumnForm }
---
+++ features/dashboard/dashboard-column/hooks/use-delete-dashboard-column-form.ts
@@ -0,0 +1,21 @@
+import { FormEvent } from "react"
+import { useDeleteDashboardColumn } from "@features/dashboard/dashboard-column/hooks"
+import { DashboardColumn } from "@shared/dashboard/types"
+
+interface Props extends Pick<DashboardColumn, "id"> {
+  onCloseModal: () => void
+}
+
+export default function useDeleteDashboardColumnForm({ id, onCloseModal }: Props) {
+  const deleteColumnMutation = useDeleteDashboardColumn(id)
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    await deleteColumnMutation.trigger()
+    onCloseModal()
+  }
+
+  return {
+    onSubmit,
+    isLoading: deleteColumnMutation.isMutating,
+  }
+}
