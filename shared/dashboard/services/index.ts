9c5f4742772f396106f8323455ef58b7a80e3aab
d5ng <dong5ffice@gmail.com>
2024-07-24 03:28:23 +0900

    ✨ [#32] Feat: Task Card API 추가

--- shared/dashboard/services/index.ts
+++ shared/dashboard/services/index.ts
@@ -2,5 +2,6 @@ import MemberApiInstance from "./member.api"
 import DashboardApiInstance from "./dashboard.api"
 import InviteApiInstance from "./invite.api"
 import DashboardColumnApiInstance from "./dashboard-column.api"
+import TaskCardApiInstance from "./task-card.api"
 
-export { InviteApiInstance, MemberApiInstance, DashboardApiInstance, DashboardColumnApiInstance }
+export { InviteApiInstance, MemberApiInstance, DashboardApiInstance, DashboardColumnApiInstance, TaskCardApiInstance }
