4b0e44211bea106d7e1a4d4aac57e84c25449a2a
d5ng <dong5ffice@gmail.com>
2024-07-25 21:08:51 +0900

    ✨ [#56] Feat: invite pagination 추가

--- shared/dashboard/hooks/store/index.ts
+++ shared/dashboard/hooks/store/index.ts
@@ -1,5 +1,6 @@
 import useMemberStore from "./member.store"
 import useMemberPageStore from "./member-page-store"
 import useDashboardPageStore from "./dashboard-page-store"
+import useInvitePageStore from "./invite-page-store"
 
-export { useMemberStore, useMemberPageStore, useDashboardPageStore }
+export { useMemberStore, useMemberPageStore, useDashboardPageStore, useInvitePageStore }
