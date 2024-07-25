aaed83c0cce12145e3e701d59267686f8811e8e5
d5ng <dong5ffice@gmail.com>
2024-07-25 19:00:16 +0900

    ✨ [#32] Feat: Comment API 추가

---
+++ shared/dashboard/services/dashboard-task-card-comment.api.ts
@@ -0,0 +1,24 @@
+import { axiosInstance } from "@/config"
+import { Comments, CreateComment, UpdateCommnet } from "@shared/dashboard/types"
+
+class CommentAPI {
+  async getComments(url: string) {
+    return (await axiosInstance.get<Comments>(url)).data
+  }
+
+  async createComment(url: string, { arg }: { arg: CreateComment }) {
+    return await axiosInstance.post(url, arg)
+  }
+
+  async updateComment(url: string, { arg }: { arg: UpdateCommnet }) {
+    return await axiosInstance.put(url, arg)
+  }
+
+  async deleteComment(url: string) {
+    return await axiosInstance.delete(url)
+  }
+}
+
+const CommentApiInstance = new CommentAPI()
+
+export default CommentApiInstance
--- shared/dashboard/services/index.ts
+++ shared/dashboard/services/index.ts
@@ -3,5 +3,13 @@ import DashboardApiInstance from "./dashboard.api"
 import InviteApiInstance from "./invite.api"
 import DashboardColumnApiInstance from "./dashboard-column.api"
 import TaskCardApiInstance from "./task-card.api"
+import CommentApiInstance from "./dashboard-task-card-comment.api"
 
-export { InviteApiInstance, MemberApiInstance, DashboardApiInstance, DashboardColumnApiInstance, TaskCardApiInstance }
+export {
+  InviteApiInstance,
+  MemberApiInstance,
+  DashboardApiInstance,
+  DashboardColumnApiInstance,
+  TaskCardApiInstance,
+  CommentApiInstance,
+}
