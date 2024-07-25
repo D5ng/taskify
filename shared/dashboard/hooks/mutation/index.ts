7d45492d0e2a358b4d17abe2d796b5548d5bc744
d5ng <dong5ffice@gmail.com>
2024-07-25 19:17:14 +0900

    ✨ [#32] Feat: 대시보드 상세 댓글 데이터 패칭 및 mutation 추가

--- shared/dashboard/hooks/mutation/index.ts
+++ shared/dashboard/hooks/mutation/index.ts
@@ -1,3 +1,4 @@
 export * from "./dashboard.mutation"
 export * from "./dashboard-column.mutation"
 export * from "./card.mutation"
+export * from "./comment.mutation"
--- shared/dashboard/hooks/queries/index.ts
+++ shared/dashboard/hooks/queries/index.ts
@@ -1,3 +1,4 @@
 export * from "./dashboard.query"
 export * from "./dashboard-column.query"
 export * from "./card.query"
+export * from "./comment.query"
