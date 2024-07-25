02ef6c1f01333f039dbb55249490f7d05d20c31b
d5ng <dong5ffice@gmail.com>
2024-07-25 20:24:41 +0900

    ✨ [#52] Feat: Dashboard Edit Layout 컴포넌트 추가

--- shared/dashboard/components/dashboard-layout/index.ts
+++ shared/dashboard/components/dashboard-layout/index.ts
@@ -1,5 +1,6 @@
 import DashboardLayout from "./dashboard-layout"
 import DashboardListFetcherLayout from "./dashboard-list-fetcher-layout"
 import DashboardSectionLayout from "./dashboard-section-layout"
+import DashboardEditLayout from "./dashboard-edit-layout"
 
-export { DashboardListFetcherLayout, DashboardLayout, DashboardSectionLayout }
+export { DashboardListFetcherLayout, DashboardLayout, DashboardSectionLayout, DashboardEditLayout }
