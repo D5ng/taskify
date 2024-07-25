a1e4a85514476a17bc8318b9b6bc5f9a96a4b608
d5ng <dong5ffice@gmail.com>
2024-07-25 18:34:01 +0900

    ✨ [#32] Feat:  대시보드 드롭다운 추가

--- shared/dashboard/components/dashboard-modal/index.ts
+++ shared/dashboard/components/dashboard-modal/index.ts
@@ -1,5 +1,8 @@
 import CreateDashboardModal from "./dashboard/create-dashboard-modal"
+
 import DashboardColumnEditModal from "./dashboard-column/dashboard-column-edit-modal"
 import DashboardColumnDeleteModal from "./dashboard-column/dashboard-column-delete-modal"
 
-export { CreateDashboardModal, DashboardColumnEditModal, DashboardColumnDeleteModal }
+import TaskCardCreateModal from "./dashboard-task-card/task-card-create-modal"
+
+export { CreateDashboardModal, DashboardColumnEditModal, DashboardColumnDeleteModal, TaskCardCreateModal }
