035f5ef3e116d7c0503ea5a38d4c516d5d53b61b
d5ng <dong5ffice@gmail.com>
2024-07-26 11:43:43 +0900

    ✨ [#63] Feat: Dashboard Header Dropdown 메뉴 기능 추가

--- shared/dashboard/components/dashboard-header/header-profile/dashboard-header-profile.tsx
+++ shared/dashboard/components/dashboard-header/header-profile/dashboard-header-profile.tsx
@@ -3,12 +3,15 @@ import { useAuthStore } from "@common/hooks"
 import Dropdown from "@common/components/ui/dropdown"
 import Avatar from "@common/components/ui/avatar"
 import classes from "./dashboard-header-profile.module.css"
+import { useRouter } from "next/router"
 
 export default function DashboardHeaderProfile() {
+  const router = useRouter()
   const nickname = useAuthStore.use.nickname()
   const profileImage = useAuthStore.use.profileImageUrl()
 
   const targetValidation = (target: Element) => !!target.closest("button")
+
   return (
     <Dropdown callback={targetValidation} className={classes["header-profile"]}>
       <Dropdown.Trigger>
@@ -18,8 +21,8 @@ export default function DashboardHeaderProfile() {
         </Avatar>
       </Dropdown.Trigger>
       <Dropdown.Menu>
-        <Dropdown.MenuItem>관리</Dropdown.MenuItem>
-        <Dropdown.MenuItem>마이 페이지</Dropdown.MenuItem>
+        <Dropdown.MenuItem onClick={() => router.push(`/dashboard/my`)}>내 대시보드</Dropdown.MenuItem>
+        <Dropdown.MenuItem onClick={() => router.push(`/dashboard/mypage`)}>마이 페이지</Dropdown.MenuItem>
       </Dropdown.Menu>
     </Dropdown>
   )
