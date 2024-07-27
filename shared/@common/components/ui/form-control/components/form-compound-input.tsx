d081c7a5e99d0db0b80ca73588731cd6d7631536
d5ng <dong5ffice@gmail.com>
2024-07-27 11:49:06 +0900

    ♻️  [#65] Feat: className undefined 해결

--- shared/@common/components/ui/form-control/components/form-compound-input.tsx
+++ shared/@common/components/ui/form-control/components/form-compound-input.tsx
@@ -4,7 +4,7 @@ import classes from "../index.module.css"
 
 export default function Input(props: InputHTMLAttributes<HTMLInputElement>) {
   const formControlContext = useFormControlContext()
-  const isInputFile = props.type === "file" && classes["upload-input"]
+  const isInputFile = props.type === "file" ? classes["upload-input"] : ""
   return (
     <input
       {...props}
