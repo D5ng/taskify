8680417344c635b7fd0ec1d7b1a7bf7fa4121931
d5ng <dong5ffice@gmail.com>
2024-08-02 03:40:11 +0900

    ✨ [#81] Feat: Dashboard Edit Form Hook 기능 변경

--- features/dashboard/dashboard-edit/hooks/use-edit-dashboard-form.ts
+++ features/dashboard/dashboard-edit/hooks/use-edit-dashboard-form.ts
@@ -2,6 +2,7 @@ import { FormEventHandler } from "react"
 import { useDashboardPageStore, useUpdateDashboard } from "@/shared/dashboard/hooks"
 import { ColorChipColor } from "@/shared/@common/types"
 import { useRouterQuery } from "@/shared/@common/hooks"
+import useDashboardStore from "@/shared/dashboard/hooks/store/dashboard.store"
 
 interface Props {
   title: string
@@ -13,10 +14,12 @@ export default function useEditDashboardForm({ title, color }: Props) {
   const dashboardId = +useRouterQuery("id")
   const currentPage = useDashboardPageStore.use.currentPage()
   const updateDashboardMutation = useUpdateDashboard(dashboardId, currentPage)
+  const setDashboard = useDashboardStore.use.setDashboard()
 
   const onSubmit: FormEventHandler = async (event) => {
     event.preventDefault()
     await updateDashboardMutation.trigger({ title, color })
+    setDashboard({ title, color })
   }
 
   return {
