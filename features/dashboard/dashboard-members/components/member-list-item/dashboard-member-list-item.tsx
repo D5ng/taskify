be36d7e65a2a2a46527eddd051ffc9d4e253795b
d5ng <dong5ffice@gmail.com>
2024-08-13 03:20:40 +0900

    ✨ [#89] Feat: member-list-item 컴포넌트 수정

--- features/dashboard/dashboard-members/components/member-list-item/dashboard-member-list-item.tsx
+++ features/dashboard/dashboard-members/components/member-list-item/dashboard-member-list-item.tsx
@@ -2,8 +2,18 @@ import Avatar from "@/shared/@common/components/ui/avatar"
 import { Button } from "@/shared/@common/components/ui/button"
 import { Member } from "@/shared/dashboard/types"
 import classes from "./dashboard-member-list-item.module.css"
+import { useDeleteMember, useMemberPageStore } from "@/shared/dashboard/hooks"
+import { useRouterQuery } from "@/shared/@common/hooks"
 
 export default function DashboardMemberListItem(props: Member) {
+  const dashboardId = +useRouterQuery("id")
+  const currentPage = useMemberPageStore.use.currentPage()
+  const deleteMemberMutation = useDeleteMember(dashboardId, currentPage, props.id)
+
+  const handleDeleteMember = async () => {
+    await deleteMemberMutation.trigger()
+  }
+
   return (
     <div className={classes["list-layout"]}>
       <div className={classes["list-layout__profile"]}>
@@ -13,7 +23,13 @@ export default function DashboardMemberListItem(props: Member) {
         </Avatar>
       </div>
       <div>
-        <Button buttonStyle="secondary" size="small">
+        <Button
+          buttonStyle="secondary"
+          size="small"
+          isLoading={deleteMemberMutation.isMutating}
+          isDisabled={deleteMemberMutation.isMutating}
+          onClick={handleDeleteMember}
+        >
           삭제
         </Button>
       </div>
