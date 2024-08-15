96cbdb8a27f384861f7a51dd62703f9f5c6db377
d5ng <dong5ffice@gmail.com>
2024-08-15 11:37:36 +0900

    ✨ [#100] Feat: type 병합

--- features/auth/types/index.ts
+++ features/auth/types/index.ts
@@ -1,1 +1,12 @@
-export * from "./signup.type"
+export interface FormValues {
+  email: string
+  nickname: string
+  password: string
+  passwordConfirm: string
+}
+
+export interface SignupValues extends FormValues {}
+
+export interface SigninValues extends Pick<FormValues, "email" | "password"> {
+  form: string
+}
