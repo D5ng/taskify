b23c52c6c002d2e4de2bdbd0507d4613aeda3272
d5ng <dong5ffice@gmail.com>
2024-08-16 07:00:37 +0900

    🔥  [#103] Remove: Color Chip 제거

--- shared/@common/types/color-chip.type.ts
+++ shared/@common/types/color-chip.type.ts
@@ -1,12 +1,3 @@
 export type ColorChipColor = "#7AC555" | "#760DDE" | "#FFA500" | "#76A5EA" | "#E876EA"
 
 export type ColorChipColors = ColorChipColor[]
-
-export interface ColorChipsProps {
-  selectedColorChip: ColorChipColor
-  onSelectedColorChip: (selectedColor: ColorChipColor) => void
-}
-
-export interface ColorChipItemProps extends ColorChipsProps {
-  color: ColorChipColor
-}
