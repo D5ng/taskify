1123a7de6de8e5996f1993fa2277a3449488147d
d5ng <dong5ffice@gmail.com>
2024-08-16 07:43:08 +0900

    ✨ [#89] Feat: valdiate 로직 변경

--- features/dashboard/dashboard-invite/logic/index.ts
+++ features/dashboard/dashboard-invite/logic/index.ts
@@ -1,3 +1,4 @@
+import { Invite } from "@/shared/dashboard/types"
 import { emailValidation } from "@common/utils/validation"
 import { DefaultValues } from "@features/dashboard/dashboard-invite/types"
 
@@ -5,10 +6,13 @@ export const defaultValues: DefaultValues = {
   email: "",
 }
 
-export const validate = (values: DefaultValues) => {
+export const validate = (values: DefaultValues, inviteData: Invite[] | undefined) => {
   const error: Partial<DefaultValues> = {}
 
   error.email = emailValidation(values.email)
+
+  const existingInvite = inviteData?.find((invite) => invite.invitee.email === values.email)
+  if (existingInvite) error.email = "이미 등록된 이메일입니다."
 
   return error
 }
