3c22d25c90f583fb5c70b03b892fe20531a5a30e
d5ng <dong5ffice@gmail.com>
2024-08-16 06:58:24 +0900

    ✨ [#103] Feat: dashboardEditForm 컴포넌트 변경

--- features/dashboard/dashboard-edit/components/edit-form/dashboard-edit-form.tsx
+++ features/dashboard/dashboard-edit/components/edit-form/dashboard-edit-form.tsx
@@ -2,8 +2,7 @@ import React from "react"
 import { useFetchDashboard } from "@/shared/dashboard/hooks"
 import { Button } from "@common/components/ui/button"
 import { ColorChipList } from "@common/components/color-chip"
-import { useForm, useSelect } from "@common/hooks"
-import { ColorChipColor } from "@common/types"
+import { useForm } from "@common/hooks"
 import { DefaultValues } from "@features/dashboard/dashboard-edit/types"
 import { validate } from "@features/dashboard/dashboard-edit/logic"
 import { useEditDashboardForm } from "@features/dashboard/dashboard-edit/hooks"
@@ -16,15 +15,11 @@ interface Props {
 
 export default function DashboardEditForm(props: Props) {
   const dashboardQuery = useFetchDashboard(props.dashboardId)
-  const colorChipStates = useSelect<ColorChipColor>(dashboardQuery.data!.color)
-
-  const defaultValues = {
-    title: dashboardQuery.data!.title,
-    color: colorChipStates.selectedItem,
-  }
-
-  const { register, handleSubmit, fieldError, formStates, handleSetError } = useForm<DefaultValues>({
-    defaultValues,
+  const { register, handleSubmit, fieldError, formStates, handleSetError, handleSelect } = useForm<DefaultValues>({
+    defaultValues: {
+      title: dashboardQuery.data!.title,
+      color: dashboardQuery.data!.color,
+    },
     validate,
   })
 
@@ -34,18 +29,13 @@ export default function DashboardEditForm(props: Props) {
     <form onSubmit={handleSubmit(onSubmit)}>
       <h2 className={classes["new-dashboard-title"]}>{dashboardQuery.data!.title}</h2>
       <div className={classes["color-chips"]}>
-        <ColorChipList
-          onSelectedColorChip={colorChipStates.onSelectedItem}
-          selectedColorChip={colorChipStates.selectedItem}
-        />
+        <ColorChipList onChange={handleSelect("color")} value={formStates.formValues.color} />
       </div>
-
       <FormControl type="form" id="title" hasError={fieldError}>
         <FormControl.Label>대시보드 이름</FormControl.Label>
         <FormControl.Input type="text" placeholder="대시보드 이름을 입력해주세요" {...register("title")} />
         <FormControl.ErrorMessage />
       </FormControl>
-
       <div className={classes["new-dashboard-button"]}>
         <Button
           isLoading={formStates.isSubmitting}
