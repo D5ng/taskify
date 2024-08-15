534f47eb5f2066cd6aa176089f8fa963e81a3c11
d5ng <dong5ffice@gmail.com>
2024-08-15 14:24:49 +0900

    ✨ [#103] Feat: dashboardEditForm 컴포넌트 변경

--- features/dashboard/dashboard-edit/components/edit-form/dashboard-edit-form.tsx
+++ features/dashboard/dashboard-edit/components/edit-form/dashboard-edit-form.tsx
@@ -1,12 +1,13 @@
 import React from "react"
-import { useEditDashboardForm } from "@/features/dashboard/dashboard-edit/hooks"
 import { useFetchDashboard } from "@/shared/dashboard/hooks"
 import { Button } from "@common/components/ui/button"
 import { ColorChipList } from "@common/components/color-chip"
-import { FormControlDashboardName } from "@common/components/form-control"
-import { useInput, useSelect } from "@common/hooks"
+import { useForm, useSelect } from "@common/hooks"
 import { ColorChipColor } from "@common/types"
-import { isNotEmptyValidation } from "@common/utils/validation"
+import { DefaultValues } from "@features/dashboard/dashboard-edit/types"
+import { validate } from "@features/dashboard/dashboard-edit/logic"
+import { useEditDashboardForm } from "@features/dashboard/dashboard-edit/hooks"
+import FormControl from "@common/components/ui/form-control"
 import classes from "./dashboard-edit-form.module.css"
 
 interface Props {
@@ -15,16 +16,22 @@ interface Props {
 
 export default function DashboardEditForm(props: Props) {
   const dashboardQuery = useFetchDashboard(props.dashboardId)
-  const inputStates = useInput(isNotEmptyValidation, dashboardQuery.data!.title)
   const colorChipStates = useSelect<ColorChipColor>(dashboardQuery.data!.color)
 
-  const formStates = useEditDashboardForm({
-    title: inputStates.inputValue,
+  const defaultValues = {
+    title: dashboardQuery.data!.title,
     color: colorChipStates.selectedItem,
+  }
+
+  const { register, handleSubmit, fieldError, formStates, handleSetError } = useForm<DefaultValues>({
+    defaultValues,
+    validate,
   })
 
+  const onSubmit = useEditDashboardForm(handleSetError)
+
   return (
-    <form onSubmit={formStates.onSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <h2 className={classes["new-dashboard-title"]}>{dashboardQuery.data!.title}</h2>
       <div className={classes["color-chips"]}>
         <ColorChipList
@@ -32,11 +39,17 @@ export default function DashboardEditForm(props: Props) {
           selectedColorChip={colorChipStates.selectedItem}
         />
       </div>
-      <FormControlDashboardName {...inputStates} type="form" id="dashboard-name" />
+
+      <FormControl type="form" id="title" hasError={fieldError}>
+        <FormControl.Label>대시보드 이름</FormControl.Label>
+        <FormControl.Input type="text" placeholder="대시보드 이름을 입력해주세요" {...register("title")} />
+        <FormControl.ErrorMessage />
+      </FormControl>
+
       <div className={classes["new-dashboard-button"]}>
         <Button
-          isLoading={formStates.isLoading}
-          isDisabled={formStates.isLoading}
+          isLoading={formStates.isSubmitting}
+          isDisabled={formStates.hasFormError}
           size="small"
           buttonStyle="primary"
           type="submit"
