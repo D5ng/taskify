d5aeb55f0b28ebd1ac96552a0de3fde44fcaf35e
d5ng <dong5ffice@gmail.com>
2024-08-15 11:35:04 +0900

    ✨  [#86] Feat: setError 타입 추가

--- shared/@common/types/form.type.ts
+++ shared/@common/types/form.type.ts
@@ -10,3 +10,5 @@ export type FormFields = Record<string, any>
 export type FieldElement = HTMLInputElement | HTMLTextAreaElement
 
 export type SubmitHandler<T> = (values: T) => void | Promise<any>
+
+export type SetError<T> = (error: Partial<T>) => void
