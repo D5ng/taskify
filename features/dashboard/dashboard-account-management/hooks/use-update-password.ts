283c0946f628625327039f34720a5c37ddaa5638
d5ng <dong5ffice@gmail.com>
2024-08-16 09:47:30 +0900

    🐛  [ #105] Fix: useUpdatePassword 에러 수정

--- features/dashboard/dashboard-account-management/hooks/use-update-password.ts
+++ features/dashboard/dashboard-account-management/hooks/use-update-password.ts
@@ -1,52 +1,24 @@
-import { FormEventHandler, useState } from "react"
-import { AxiosError } from "axios"
+import { isAxiosError } from "axios"
 import { AuthApiInstance } from "@common/services"
-import type { InputStates } from "@common/components/ui/form-control"
+import { SetError } from "@common/types"
+import { DefaultValues } from "../types"
 
-interface UesUpdatePassword {
-  currentPasswordState: InputStates
-  newPasswordState: InputStates
-  newPasswordConfirmState: InputStates
-}
-
-export default function useUpdatePassword({
-  currentPasswordState,
-  newPasswordState,
-  newPasswordConfirmState,
-}: UesUpdatePassword) {
-  const [hasFormError, setHasFormError] = useState<any>("")
-  const [isLoading, setIsLoading] = useState(false)
-
-  const isFormValid = currentPasswordState.isValid && newPasswordState.isValid && newPasswordConfirmState.isValid
-
-  const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
-    event.preventDefault()
-
-    if (!isFormValid) return
-    setIsLoading(true)
-
-    const data = {
-      password: currentPasswordState.inputValue,
-      newPassword: newPasswordState.inputValue,
-    }
-
+export default function useUpdatePassword(setError: SetError<DefaultValues>) {
+  const onSubmit = async (values: DefaultValues) => {
     try {
-      await AuthApiInstance.updatePassword(data)
-      currentPasswordState.handleInputReset!()
-      newPasswordState.handleInputReset!()
-      newPasswordConfirmState.handleInputReset!()
-      setIsLoading(false)
-    } catch (err) {
-      const error = err as AxiosError<{ message: string }>
-      setHasFormError(error.response?.data.message || "알 수 없는 에러가 발생했어요.")
-      setIsLoading(false)
+      await AuthApiInstance.updatePassword({
+        password: values.currentPassword,
+        newPassword: values.newPassword,
+      })
+    } catch (error) {
+      if (isAxiosError(error) && error.response) {
+        setError({
+          newPassword: error.response.data.message,
+          newPasswordConfirm: error.response.data.message,
+        })
+      }
     }
   }
 
-  return {
-    isLoading,
-    hasFormError,
-    isFormValid,
-    handleSubmit,
-  }
+  return onSubmit
 }
