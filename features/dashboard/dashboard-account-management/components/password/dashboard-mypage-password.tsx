3b496845dab0d671d3f661e7cd0d7345dffbace1
d5ng <dong5ffice@gmail.com>
2024-08-16 09:47:49 +0900

    🐛  [ #105] Fix: 마이페이지 패스워드 컴포넌트 에러 수정

--- features/dashboard/dashboard-account-management/components/password/dashboard-mypage-password.tsx
+++ features/dashboard/dashboard-account-management/components/password/dashboard-mypage-password.tsx
@@ -1,35 +1,51 @@
-import React from "react"
-import { useInput } from "@common/hooks"
 import { Button } from "@common/components/ui/button"
+import FormControl from "@common/components/ui/form-control"
+import { useForm } from "@common/hooks"
 import { useUpdatePassword } from "@features/dashboard/dashboard-account-management/hooks"
-import { passwordConfirmValidation, passwordValidation } from "@common/utils/validation"
-import {
-  FormControlCurrentPassword,
-  FormControlNewPassword,
-  FormControlNewPasswordConfirm,
-} from "@common/components/form-control"
+import { PasswordDefaultValues } from "@features/dashboard/dashboard-account-management/types"
+import { Password } from "@features/dashboard/dashboard-account-management/logic"
 import classes from "./dashboard-mypage-password.module.css"
 
 export default function DashboardMyPagePassword() {
-  const currentPasswordState = useInput(passwordValidation)
-  const newPasswordState = useInput(passwordValidation)
-  const newPasswordConfirmState = useInput(passwordConfirmValidation.bind(null, newPasswordState.inputValue))
-
-  const { isLoading, hasFormError, isFormValid, handleSubmit } = useUpdatePassword({
-    currentPasswordState,
-    newPasswordState,
-    newPasswordConfirmState,
+  const { formStates, register, fieldError, handleSubmit, handleSetError } = useForm<PasswordDefaultValues>({
+    defaultValues: Password.defaultValues,
+    validate: Password.validate,
   })
+
+  const onSubmit = useUpdatePassword(handleSetError)
 
   return (
     <>
       <h2 className={classes.title}>비밀번호 변경</h2>
-      <form className={classes["password-form"]} onSubmit={handleSubmit}>
-        <FormControlCurrentPassword {...currentPasswordState} type="form" id="current-password" />
-        <FormControlNewPassword {...newPasswordState} type="form" id="new-password" />
-        <FormControlNewPasswordConfirm {...newPasswordConfirmState} type="form" id="new-password-confirm" />
+      <form className={classes["password-form"]} onSubmit={handleSubmit(onSubmit)}>
+        <FormControl type="form" id="currentPassword" hasError={fieldError}>
+          <FormControl.Label>현재 비밀번호</FormControl.Label>
+          <FormControl.Input type="password" placeholder="비밀번호를 입력해주세요" {...register("currentPassword")} />
+          <FormControl.ErrorMessage />
+        </FormControl>
+        <FormControl type="form" id="newPassword" hasError={fieldError}>
+          <FormControl.Label>새 비밀번호</FormControl.Label>
+          <FormControl.Input type="password" placeholder="비밀번호를 입력해주세요" {...register("newPassword")} />
+          <FormControl.ErrorMessage />
+        </FormControl>
+        <FormControl type="form" id="newPasswordConfirm" hasError={fieldError}>
+          <FormControl.Label>새 비밀번호 확인</FormControl.Label>
+          <FormControl.Input
+            type="password"
+            placeholder="비밀번호를 입력해주세요"
+            {...register("newPasswordConfirm")}
+          />
+          <FormControl.ErrorMessage />
+        </FormControl>
+
         <div className={classes["password-form__submit"]}>
-          <Button type="submit" buttonStyle="primary" size="small" isDisabled={isFormValid} isLoading={isLoading}>
+          <Button
+            type="submit"
+            buttonStyle="primary"
+            size="small"
+            isDisabled={formStates.hasFormError}
+            isLoading={formStates.isSubmitting}
+          >
             저장
           </Button>
         </div>
