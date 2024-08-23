56f75c9d81de3a5e02b978f485b565e7fabcc0cd
d5ng <dong5ffice@gmail.com>
2024-08-24 01:57:10 +0900

    ✨ [#124] Feat: Commnet Update Form  훅 추가

--- features/dashboard/dashboard-task-detail/components/detail-comment/comment-update/task-detail-update-comment.tsx
+++ features/dashboard/dashboard-task-detail/components/detail-comment/comment-update/task-detail-update-comment.tsx
@@ -1,5 +1,6 @@
 import type { Comment } from "@shared/dashboard/types"
 import { TaskDetailCommentForm } from "@features/dashboard/dashboard-task-detail/components"
+import { useCommentUpdateForm } from "@features/dashboard/dashboard-task-detail/hooks"
 import classes from "./task-detail-update-comment.module.css"
 
 interface Props extends Pick<Comment, "content" | "cardId" | "id"> {
@@ -7,9 +8,14 @@ interface Props extends Pick<Comment, "content" | "cardId" | "id"> {
 }
 
 export default function TaskDetailUpdateCommentForm(props: Props) {
+  const onSubmit = useCommentUpdateForm({
+    cardId: props.cardId,
+    commentId: props.id,
+    onCloseToggle: props.onCloseToggle,
+  })
   return (
     <div className={classes["update-form"]}>
-      <TaskDetailCommentForm cardId={props.cardId} columnId={props.id} />
+      <TaskDetailCommentForm cardId={props.cardId} columnId={props.id} content={props.content} onSubmit={onSubmit} />
     </div>
   )
 }
