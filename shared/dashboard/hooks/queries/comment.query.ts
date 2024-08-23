d93c82d9eebd7606f27c0571475e1c459b20858e
d5ng <dong5ffice@gmail.com>
2024-08-24 00:53:22 +0900

    ✨ [#122] Feat: 무한 스크롤 기능 구현

--- shared/dashboard/hooks/queries/comment.query.ts
+++ shared/dashboard/hooks/queries/comment.query.ts
@@ -1,7 +1,18 @@
-import useSWR from "swr"
+import useSWRInfinite from "swr/infinite"
 import { CommentApiInstance } from "@shared/dashboard/services"
+import type { Comments } from "@shared/dashboard/types"
+
+const getKey = (cardId: number) => (pageIndex: number, previousPageData: Comments) => {
+  if (previousPageData && !previousPageData.cursorId) return null
+  if (pageIndex === 0) return `comments?cardId=${cardId}&size=3`
+  return `comments?cardId=${cardId}&size=3&cursorId=${previousPageData.cursorId}`
+}
 
 export function useFetchComments(cardId: number) {
-  const fetcher = (url: string) => CommentApiInstance.getComments(url)
-  return useSWR(`comments?cardId=${cardId}`, fetcher)
+  const commentsQuery = useSWRInfinite(getKey(cardId), CommentApiInstance.getComments)
+  const lastPage = commentsQuery.data?.[commentsQuery.data?.length - 1]
+  return {
+    ...commentsQuery,
+    lastPage,
+  }
 }
