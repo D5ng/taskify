0312418b304e3fe2c4ae3ba8a188a527b1f29f68
d5ng <dong5ffice@gmail.com>
2024-08-31 04:47:25 +0900

    ♻️  [#136] Refactor: Invited Mutation 리팩토링

--- features/dashboard/dashboard-invited/components/invited-list-item/dashboard-invited-list-item.tsx
+++ features/dashboard/dashboard-invited/components/invited-list-item/dashboard-invited-list-item.tsx
@@ -1,35 +1,24 @@
 import { Button } from "@common/components/ui"
-import { useUpdateInvitedDashboard } from "@/shared/dashboard/hooks"
+import { useAcceptInvite, useRefuseInvite } from "@/shared/dashboard/hooks"
 import type { InvitedDashboard } from "@/shared/dashboard/types"
 import classes from "./dashboard-invited-list-item.module.css"
 
 export default function DashboardInvitedListItem(props: InvitedDashboard) {
-  const inviteMutation = useUpdateInvitedDashboard(props.id)
+  const acceptInviteMutation = useAcceptInvite(props.id)
+  const refuseInviteMutation = useRefuseInvite(props.id)
 
-  const inviteAccept = () => inviteMutation.trigger({ inviteAccepted: true })
-  const inviteRefuse = () => inviteMutation.trigger({ inviteAccepted: false })
+  const inviteAccept = () => acceptInviteMutation.trigger({ inviteAccepted: true })
+  const inviteRefuse = () => refuseInviteMutation.trigger({ inviteAccepted: false })
 
   return (
     <li className={classes["invite-list__item"]}>
       <span>{props.dashboard.title}</span>
       <span>{props.inviter.nickname}</span>
       <span className={classes["invite-list__button"]}>
-        <Button
-          disabled={false}
-          isLoading={inviteMutation.isMutating}
-          buttonStyle="primary"
-          size="small"
-          onClick={inviteAccept}
-        >
+        <Button isLoading={acceptInviteMutation.isMutating} buttonStyle="primary" size="small" onClick={inviteAccept}>
           수락
         </Button>
-        <Button
-          disabled={false}
-          isLoading={inviteMutation.isMutating}
-          buttonStyle="outline"
-          size="small"
-          onClick={inviteRefuse}
-        >
+        <Button isLoading={refuseInviteMutation.isMutating} buttonStyle="outline" size="small" onClick={inviteRefuse}>
           거절
         </Button>
       </span>
