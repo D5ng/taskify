4d65f008bc986221c4c16b4a1eb33f816ebd0bb9
d5ng <dong5ffice@gmail.com>
2024-08-31 02:31:04 +0900

    ✨ [#132] Feat: Avatar 이름 스켈레톤 적용

--- shared/@common/components/ui/avatar/components/avatar-name.tsx
+++ shared/@common/components/ui/avatar/components/avatar-name.tsx
@@ -4,6 +4,6 @@ import classes from "../avatar.module.css"
 
 export default function AvatarName() {
   const avatarContext = useAvatarContext()
-
+  if (!avatarContext.nickname) return <span className={classes["avatar-name__skeleton"]}></span>
   return <span className={classes["avatar-name"]}>{avatarContext.nickname}</span>
 }
