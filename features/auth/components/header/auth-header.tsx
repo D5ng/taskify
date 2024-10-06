616ed3afb99367aff72ad18cce5f59d924f16cd8
d5ng <dong5ffice@gmail.com>
2024-10-07 02:29:41 +0900

    💄 [#165] Design: 로그인, 회원가입 페이지 반응형 작업

--- features/auth/components/header/auth-header.tsx
+++ features/auth/components/header/auth-header.tsx
@@ -4,9 +4,14 @@ import classes from "./auth-header.module.css"
 export default function AuthHeader() {
   return (
     <>
-      <div className={classes["image-wrapper"]}>
-        <Image src="images/logo/logo-with-title-big.svg" alt="테스키파이" fill />
-      </div>
+      <Image
+        src="images/logo/logo-with-title-big.svg"
+        alt="테스키파이"
+        width="0"
+        height="0"
+        priority
+        className={classes["image-wrapper"]}
+      />
       <p className={classes["form-info"]}>첫 방문을 환영합니다!</p>
     </>
   )
