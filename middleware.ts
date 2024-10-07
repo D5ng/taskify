4c10e451eefcb877a5f449e681da82e35f25e597
d5ng <dong5ffice@gmail.com>
2024-10-07 11:45:04 +0900

    미들웨어 수정

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,16 @@
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
+export const PROTECTED_PAGE = ["/dashboard"]
 export const RESTRICT_AUTH_PAGE = ["/auth/signin", "/auth/signup"]
 
 export function middleware(request: NextRequest) {
   const cookie = request.cookies.get("token")?.value || ""
   const pathname = request.nextUrl.pathname
   const isRestrictAuthPage = RESTRICT_AUTH_PAGE.some((page) => pathname.includes(page))
+  const isProtectedPage = PROTECTED_PAGE.some((page) => pathname.includes(page))
 
-  if (!cookie) return NextResponse.redirect(new URL("/auth/signin", request.url))
+  if (!cookie && isProtectedPage) return NextResponse.redirect(new URL("/auth/signin", request.url))
   if (cookie && (isRestrictAuthPage || pathname === "/"))
     return NextResponse.redirect(new URL("/dashboard/my", request.url))
 
