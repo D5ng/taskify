a03a36de7dfc4e6ad95d002e0d8c84d5a4b4aa13
d5ng <dong5ffice@gmail.com>
2024-10-23 11:12:21 +0900

    🐛 [#188] Fix: 내가 만든 대시보드 페이지인지 구분이 안되는 문제

--- features/dashboard/dashboard-list/components/list-item/dashboard-list-item.tsx
+++ features/dashboard/dashboard-list/components/list-item/dashboard-list-item.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import Image from "next/image"
+import { useAuthStore } from "@common/hooks"
 import { Dashboard } from "@shared/dashboard/types"
 import classes from "./dashboard-list-item.module.css"
 
@@ -8,6 +9,8 @@ interface Props {
 }
 
 export default function DashboardListItem(props: Props) {
+  const myUserId = useAuthStore.use.id()
+  const isAdmin = props.dashboard.userId === myUserId
   return (
     <li>
       <Link href={`/dashboard/${props.dashboard.id}`} className={classes["dashboard-list-item"]}>
@@ -15,7 +18,10 @@ export default function DashboardListItem(props: Props) {
           className={classes["dashboard-list-color-chip"]}
           style={{ backgroundColor: props.dashboard.color }}
         ></span>
-        <span className={classes["dashboard-list-item__title"]}>{props.dashboard.title}</span>
+        <span className={classes["dashboard-list-item__title"]}>
+          {props.dashboard.title}
+          {isAdmin && <Image src="/images/icons/crown.svg" alt="" width={20} height={16} />}
+        </span>
         <div className={classes["dashboard-list-item__image"]}>
           <Image src="/images/icons/arrow-right.svg" alt="" fill />
         </div>
