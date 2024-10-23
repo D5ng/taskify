6bc16997d66b2c8525582313657dc4ee2d239a15
d5ng <dong5ffice@gmail.com>
2024-10-23 09:44:12 +0900

    🐛 [#179] Fix: 대시보드 페이지네이션 에러

--- features/dashboard/dashboard-list/components/pagination/dashboard-list-pagination.tsx
+++ features/dashboard/dashboard-list/components/pagination/dashboard-list-pagination.tsx
@@ -1,24 +1,15 @@
 import { Pagination } from "@common/components/ui"
 import { useFetchDashboards, useDashboardPageStore } from "@shared/dashboard/hooks"
 import { DASHBAORD_POST_COUNT } from "@features/dashboard/dashboard-list/constants"
-import { useEffect, useState } from "react"
 
 export default function DashboardListPagination() {
-  // const [isClient, setIsClient] = useState(false)
-  // const [currentPage, setCurrentPage] = useState(1)
   const currentPage = useDashboardPageStore.use.currentPage()
   const setCurrentPage = useDashboardPageStore.use.setCurrentPage()
-  // const dashboardsQuery = useFetchDashboards(currentPage)
-  // const maxPage = Math.ceil((dashboardsQuery.data?.totalCount || 1) / DASHBAORD_POST_COUNT)
-
-  // useEffect(() => {
-  //   setIsClient(true)
-  // }, [])
-
-  // if (!isClient) return null
+  const dashboardsQuery = useFetchDashboards(currentPage)
+  const maxPage = Math.ceil((dashboardsQuery.data?.totalCount || 1) / DASHBAORD_POST_COUNT)
 
   return (
-    <Pagination value={{ maxPage: 3, currentPage, setCurrentPage }}>
+    <Pagination value={{ maxPage: maxPage, currentPage, setCurrentPage }}>
       <Pagination.Count />
       <Pagination.ButtonLayout>
         <Pagination.Prev />
